cf3f637e81166666776f65ec711ead644c9af1e7
huynhtuandat05december <huynhtuandat05december@gmail.com>
2022-05-25 22:57:53 +0700

    Zombies attack their victims

---
+++ Solidity/zombies-attack-their-victims/zombiefeeding.sol
@@ -0,0 +1,53 @@
+pragma solidity >=0.5.0 <0.6.0;
+import "./zombiefactory.sol";
+
+//1.Interface
+//By including this interface in our dapp's code our contract knows what the other contract's functions look like, how to call them, and what sort of response to expect
+// Function external:external is similar to public, except that these functions can ONLY be called outside the contract — they can't be called by other functions inside that contract
+contract KittyInterface {
+    function getKitty(uint256 _id)
+        external
+        view
+        returns (
+            bool isGestating,
+            bool isReady,
+            uint256 cooldownIndex,
+            uint256 nextActionAt,
+            uint256 siringWithId,
+            uint256 birthTime,
+            uint256 matronId,
+            uint256 sireId,
+            uint256 generation,
+            uint256 genes
+        );
+}
+
+contract ZombieFeeding is ZombieFactory {
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
+    // Initialize kittyContract here using `ckAddress` from above
+    function feedAndMultiply(
+        uint256 _zombieId,
+        uint256 _targetDna,
+        string memory _species
+    ) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        if (
+            keccak256(abi.encodePacked(_species)) ==
+            keccak256(abi.encodePacked("kitty"))
+        ) {
+            newDna = newDna - (newDna % 100) + 99;
+        }
+        _createZombie("NoName", newDna);
+    }
+
+    function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
+        uint256 kittyDna;
+        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
+    }
+}
